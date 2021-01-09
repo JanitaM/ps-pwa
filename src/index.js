09afa0b871cb69179e931002616b217c48a7a836
Janita M <jaymadr@gmail.com>
2021-01-09 17:47:41 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ ReactDOM.render(
 );
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register() below. Note this comes with some pitfalls (debugging).
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
